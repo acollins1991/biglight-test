9c7fca174324baa89c2eda263b9843db11d959af
Anthony Collins <a.r.c_@hotmail.co.uk>
2020-11-25 23:52:15 +0000

    basic sass compiling

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,5 +1,19 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+  plugins: [new MiniCssExtractPlugin()],
   entry: './src/client.js',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      }
+    ],
+  },
 };
