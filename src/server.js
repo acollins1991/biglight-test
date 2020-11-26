f6b27fd7ed9a9f9ee66ad3ce536626e33e3769ca
Anthony Collins <a.r.c_@hotmail.co.uk>
2020-11-26 09:00:43 +0000

    portfolio page populated

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const GetHompageContent = require('./serverJs/GetHompageContent');
+const GetPortfolioContent = require('./serverJs/GetPortfolioContent');
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
+// homepage
 app.get('/', async (req, res) => {
 
   const homepageContent = await new GetHompageContent().init();
@@ -15,7 +17,7 @@ app.get('/', async (req, res) => {
   const subHeadline = await homepageContent.getSubHeadline();
   const desktopImage = await homepageContent.getDesktopImage();
   const mobileImage = await homepageContent.getMobileImage();
-  
+
   res.render('pages/home', { content: {
     banner: {
       headline: headline,
@@ -27,6 +29,25 @@ app.get('/', async (req, res) => {
     }
   } });
 });
+
+// portfolio page
+app.get( '/portfolio', async (req, res) => {
+
+  const portfolioContent = await new GetPortfolioContent().init();
+  const headline = await portfolioContent.getHeadline();
+  const subHeadline = await portfolioContent.getSubHeadline();
+  const portfolioItems = await portfolioContent.getPorfolioItems();
+  // console.log(portfolioItems)
+
+  res.render('pages/portfolio', { content: {
+    banner: {
+      headline: headline,
+      subHeadline: subHeadline
+    },
+    portfolioItems
+  } })
+
+} );
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ src/serverJs/GetPortfolioContent.js
@@ -0,0 +1,34 @@
+const GoogleSheet = require('./GoogleSheet')
+
+module.exports = class GetPortfolioContent extends GoogleSheet {
+    constructor() {
+        super(565653012, 'A1:F6')
+    }
+
+    async getHeadline() {
+        return await this.sheet.getCellByA1('A4').formattedValue;
+    }
+
+    async getSubHeadline() {
+        return await this.sheet.getCellByA1('B4').formattedValue;
+    }
+
+    async getPorfolioItems() {
+        const rows = await this.sheet.getRows({ offset: 2 });
+        return rows.filter( row => {
+            if( row['image.mobileUrl'] && row['image.desktopUrl'] && row['image.title'] && row['image.description'] ) {
+                return true
+            } 
+            return false
+        } ).map( row => {
+            return {
+                image: {
+                    mobile: row['image.mobileUrl'],
+                    desktop: row['image.desktopUrl'],
+                    title: row['image.title'],
+                    description: row['image.description']
+                }
+            }
+        } )
+    }
+}
